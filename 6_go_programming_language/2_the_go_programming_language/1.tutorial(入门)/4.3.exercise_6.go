b7ffee3ba0b4c00e6197a3040d225fc5d5276285
isshe <isshe@foxmail.com>
2018-11-13 16:05:10 +0800

    20181113: 添加《Go语言圣经》第一章部分练习代码

---
+++ 6_go_programming_language/2_the_go_programming_language/1.tutorial(入门)/4.3.exercise_6.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{
+	color.White,
+	color.Black,
+	color.RGBA{0x0, 0x80, 0x0, 0xff},
+	color.RGBA{0x80, 0x0, 0x0, 0xff},
+	color.RGBA{0x0, 0x0, 0x80, 0xff},
+}
+
+//var palette = []color.Color{color.White, color.White}
+
+const (
+	WhiteIndex = 0
+	BlackIndex = 1
+	GreenIndex = 2
+	RedIndex   = 3
+	BludIndex  = 4
+)
+
+var colorIndex uint8 = RedIndex
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5),
+				size+int(y*size+0.5), colorIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
