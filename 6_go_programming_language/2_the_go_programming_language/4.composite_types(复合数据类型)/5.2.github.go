a0f18a43db94c43ba72cea26738a03fa35d4326b
i.sshe <i.sshe@foxmail.com>
2018-11-27 20:34:16 +0800

    20181127: 添加go语言圣经第六章部分代码

--- 6_go_programming_language/2_the_go_programming_language/4.composite_types(复合数据类型)/5.2.github.go
+++ 6_go_programming_language/2_the_go_programming_language/4.composite_types(复合数据类型)/5.2.github.go
@@ -1,65 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"isshe/github"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
-	"strings"
-	"time"
 )
-
-const IssuesURL = "https://api.github.com/search/issues"
-
-type IssuesSearchResult struct {
-	TotalCount int `json:"total_count"`
-	Items      []*Issue
-}
-
-type Issue struct {
-	Number   int
-	HTMLURL  string `json:"html_url"`
-	Title    string
-	State    string
-	User     *User
-	CreateAt time.Time `json:"create_at"`
-	Body     string
-}
-
-type User struct {
-	Login   string
-	HTMLURL string `json:"html_url"`
-}
-
-func SearchIssues(terms []string) (*IssuesSearchResult, error) {
-	q := url.QueryEscape(strings.Join(terms, " "))
-	fmt.Println(IssuesURL + "?q=" + q)
-	resp, err := http.Get(IssuesURL + "?q=" + q)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		resp.Body.Close()
-		return nil, fmt.Errorf("search query failed: %s", resp.Status)
-	}
-
-	//fmt.Println(resp.Body)
-	var result IssuesSearchResult
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		resp.Body.Close()
-		return nil, err
-	}
-
-	resp.Body.Close()
-	return &result, nil
-}
 
 //go run 5.2.github.go repo:golang/go is:open json decoder
 func main() {
-	result, err := SearchIssues(os.Args[1:])
+	result, err := github.SearchIssues(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
