db51bf50443cb80e444f3d6305a3d0864fc11c84
i.sshe <i.sshe@foxmail.com>
2018-11-30 18:02:16 +0800

    20181130: 添加go语言第5章部分代码

---
+++ 6_go_programming_language/2_the_go_programming_language/5.functions(函数)/6.1.squares.go
@@ -0,0 +1,16 @@
+
+func squares() func() int {
+	var x int
+	return func() int {
+		x++
+		return x * x
+	}
+}
+
+func main() {
+	f := squares()
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+}
